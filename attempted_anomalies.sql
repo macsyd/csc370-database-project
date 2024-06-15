8bd719a9be2e6c28841eca4bebe85d17805300ab
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-06-15 14:45:05 -0700

    add attempted deletion anomalies to attempted_anomalies.sql

--- attempted_anomalies.sql
+++ attempted_anomalies.sql
@@ -11,3 +11,17 @@ VALUES ('Lorem ipsum dolor', 'sit amet, consectetur adipiscing', 'PENDING', 1, 9
 -- Attempt to insert a tuple into Routes table that is owned by an EmployeeID that doesn't exist in Employee table:
 INSERT INTO `Routes` (`Destination`, ` Gateway`, ` SubnetMask`, `Owner`, ` Name`, ` Description`)
 VALUES ('24.68.123.111', '24.68.123.1', '255.255.255.0', 9999, 'Lorem ipsum dolor', 'sit amet, consectetur adipiscing');
+
+-- DELETION ANOMALIES
+
+-- Attempt to delete a tuple from Employee table that contains an EmployeeID used as the owner of at least one route:
+DELETE FROM `Employee`
+WHERE `EmployeeID` = 615;
+
+-- Attempt to delete a tuple from Employee table that contains an EmployeeID used as the creator of at least one request:
+DELETE FROM `Employee`
+WHERE `EmployeeID` = 175;
+
+-- Attempt to delete a tuple from Admin table that contains an AdminID used as the reviewer of at least one request:
+DELETE FROM `Admin`
+WHERE `AdminID` = 1;
