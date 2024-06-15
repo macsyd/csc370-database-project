9c4992902373bd7703eedde76eecf765af40d0fe
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-06-15 14:08:25 -0700

    change ReviewedBy attribute to NULL by default

--- VPNManagement.sql
+++ VPNManagement.sql
@@ -24,7 +24,7 @@ CREATE TABLE Requests (
     RouteName VARCHAR(100),
     Status ENUM('APPROVED', 'DENIED', 'PENDING'),
     RouteDescription TEXT,
-    ReviewedBy INT,
+    ReviewedBy INT DEFAULT NULL,
     CreatedBy INT,
     FOREIGN KEY (ReviewedBy) REFERENCES Admin(AdminID),
     FOREIGN KEY (CreatedBy) REFERENCES Employee(EmployeeID)
@@ -47,19 +47,19 @@ INSERT INTO Employee (EmployeeName) VALUES
 ('John Doe');
 
 -- Insert data into Admin table
-INSERT INTO Admin (AdminName) VALUES
-('Jane Smith');
+-- INSERT INTO Admin (AdminName) VALUES
+-- ('Jane Smith');
 
 -- Insert data into Requests table
-INSERT INTO Requests (RouteName, Status, RouteDescription, ReviewedBy, CreatedBy) VALUES
-('Office Network Access', 'APPROVED', 'Access to the office network for all employees.', 1, 1);
+-- INSERT INTO Requests (RouteName, Status, RouteDescription, ReviewedBy, CreatedBy) VALUES
+-- ('Office Network Access', 'APPROVED', 'Access to the office network for all employees.', 1, 1);
 
 -- Insert data into Routes table
-INSERT INTO Routes (Destination, SubnetMask, Gateway, ExpiryDate, Owner, Name, Description) VALUES
-('192.168.1.0', '255.255.255.0', '192.168.1.1', '2024-12-31 23:59:59', 1, 'Office Network', 'Access to office network resources');
+-- INSERT INTO Routes (Destination, SubnetMask, Gateway, ExpiryDate, Owner, Name, Description) VALUES
+-- ('192.168.1.0', '255.255.255.0', '192.168.1.1', '2024-12-31 23:59:59', 1, 'Office Network', 'Access to office network resources');
 
 
-SELECT * FROM Routes;
-SELECT * FROM Requests;
-SELECT * FROM Admin;
-SELECT * FROM Employee
+-- SELECT * FROM Routes;
+-- SELECT * FROM Requests;
+-- SELECT * FROM Admin;
+-- SELECT * FROM Employee
