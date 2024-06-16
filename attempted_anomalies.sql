5fb432447360792e1273da3794fd9985aee52645
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-06-15 17:12:16 -0700

    add update anomalies to attempted_anomalies.sql

--- attempted_anomalies.sql
+++ attempted_anomalies.sql
@@ -2,28 +2,56 @@
 
 -- INSERTION ANOMALIES
 
--- Attempt to insert a tuple into Requests table that was created by an EmployeeID that doesn't exist in the Employee table:
+-- Attempt to insert a tuple into Requests table that has CreatedBy set as an EmployeeID that does not exist in the Employee table:
 INSERT INTO `Requests` (`RouteName`, `RouteDescription`, `Status`,  `CreatedBy`, `ReviewedBy`)
 VALUES ('Lorem ipsum dolor', 'sit amet, consectetur adipiscing', 'PENDING', 9999, 1);
 
--- Attempt to insert a tuple into Requests table that was reviewed by an AdminID that doesn't exist in Admin table:
+-- Attempt to insert a tuple into Requests table that has ReviewedBy set as an AdminID that does not exist in Admin table:
 INSERT INTO `Requests` (`RouteName`, `RouteDescription`, `Status`,  `CreatedBy`, `ReviewedBy`)
 VALUES ('Lorem ipsum dolor', 'sit amet, consectetur adipiscing', 'PENDING', 1, 9999);
 
--- Attempt to insert a tuple into Routes table that is owned by an EmployeeID that doesn't exist in Employee table:
+-- Attempt to insert a tuple into Routes table that has Owner set as an EmployeeID that does not exist in Employee table:
 INSERT INTO `Routes` (`Destination`, ` Gateway`, ` SubnetMask`, `Owner`, ` Name`, ` Description`)
 VALUES ('24.68.123.111', '24.68.123.1', '255.255.255.0', 9999, 'Lorem ipsum dolor', 'sit amet, consectetur adipiscing');
 
 -- DELETION ANOMALIES
 
--- Attempt to delete a tuple from Employee table that contains an EmployeeID used as the owner of at least one route:
-DELETE FROM `Employee`
-WHERE `EmployeeID` = 615;
-
--- Attempt to delete a tuple from Employee table that contains an EmployeeID used as the creator of at least one request:
+-- Attempt to delete a tuple from Employee table that contains an EmployeeID that is also used for the CreatedBy of at least one tuple in Requests table:
 DELETE FROM `Employee`
 WHERE `EmployeeID` = 175;
 
--- Attempt to delete a tuple from Admin table that contains an AdminID used as the reviewer of at least one request:
+-- Attempt to delete a tuple from Admin table that contains an AdminID that is also used for the ReviewedBy of at least one tuple in Requests table:
 DELETE FROM `Admin`
 WHERE `AdminID` = 1;
+
+-- Attempt to delete a tuple from Employee table that contains an EmployeeID that is also used for the Owner of at least one tuple in Routes table:
+DELETE FROM `Employee`
+WHERE `EmployeeID` = 615;
+
+-- UPDATE ANOMALIES
+
+-- Attempt to update a tuple in Requests table to contain an AdminID that does not exist in Admin table:
+UPDATE `Requests`
+SET `ReviewedBY` = 9999
+WHERE `RequestID` = 1;
+
+-- Attempt to update a tuple in Routes table to contain an EmployeeID that does not exist in Employee table:
+UPDATE `Routes`
+SET `Owner` = 9999
+WHERE `RouteID` = 1;
+
+-- Attempt to update a tuple in Employee table to change an EmployeeID that was being used for the CreatedBy of at least one tuple in Requests:
+UPDATE `Employee`
+SET `EmployeeID` = 9999
+WHERE `EmployeeID` = 175;
+
+-- Attempt to update a tuple in Admin table to change an AdminID that was being used for the ReviewedBy of at least one tuple in Requests:
+UPDATE `Admin`
+SET `AdminID` = 9999
+WHERE `AdminID` = 1;
+
+
+-- Attempt to update a tuple in Employee table to change an EmployeeID that was being used for the Owner of at least once tuple in Routes:
+UPDATE `Employee`
+SET `EmployeeID` = 9999
+WHERE `EmployeeID` = 615;
