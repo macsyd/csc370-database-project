1afac7860b66cac1d5d19f581b9738177dc80f7d
Leo Barnes <122244932+Leonard-Barnes@users.noreply.github.com>
2024-07-13 20:45:16 -0700

    Update VPNManagement.sql
    Update code setup to create a database that matches our ERD

--- database-setup/VPNManagement.sql
+++ database-setup/VPNManagement.sql
@@ -7,6 +7,13 @@ CREATE DATABASE VPNManagement;
 -- Use the new database
 USE VPNManagement;
 
+-- Create Users table
+CREATE TABLE Employee (
+    UserID INT PRIMARY KEY,
+    EmployeeName VARCHAR(50) UNIQUE,
+    FOREIGN KEY (UserID) REFERENCES User(UserID)
+);
+
 -- Create the Employee table
 CREATE TABLE Employee (
     EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
@@ -15,66 +22,38 @@ CREATE TABLE Employee (
 
 -- Create the Admin table
 CREATE TABLE Admin (
-    AdminID INT AUTO_INCREMENT PRIMARY KEY,
-    AdminName VARCHAR(50) UNIQUE
+    UserID INT PRIMARY KEY,
+    AdminName VARCHAR(50) UNIQUE,
+    FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
 
 -- Create the Requests table
 CREATE TABLE Requests (
-    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     RequestID INT AUTO_INCREMENT PRIMARY KEY,
     RouteName VARCHAR(100),
-    Status ENUM('APPROVED', 'DENIED', 'PENDING') DEFAULT 'PENDING',
     RouteDescription TEXT,
-    ReviewedBy INT DEFAULT NULL,
+    CreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `Status` ENUM('APPROVED', 'REJECTED', 'PENDING'),
     CreatedBy INT,
-    FOREIGN KEY (ReviewedBy) REFERENCES Admin(AdminID),
-    FOREIGN KEY (CreatedBy) REFERENCES Employee(EmployeeID)
+    ReviewedBy INT,
+    FOREIGN KEY (ReviewedBy) REFERENCES Admin( UserID),
+    FOREIGN KEY (CreatedBy) REFERENCES Employee(UserID)
 );
 
 -- Create the Routes table
 CREATE TABLE Routes (
-    RouteID INT AUTO_INCREMENT PRIMARY KEY,
-    Destination VARCHAR(100) UNIQUE,
-    SubnetMask VARCHAR(100),
+    Destination VARCHAR(100) PRIMARY KEY,
     Gateway VARCHAR(100),
+    SubnetMask VARCHAR(100),
     CreationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ExpiryDate TIMESTAMP DEFAULT NULL,
-    Owner INT,
-    Name VARCHAR(100),
-    Description TEXT,
-    FOREIGN KEY (Owner) REFERENCES Employee(EmployeeID)
+    ExpiryDate TIMESTAMP,
+    `Owner` INT,
+    `Name` VARCHAR(100),
+    `Description` TEXT,
+    FOREIGN KEY (Owner) REFERENCES Employee(UserID)
 );
 
--- Create database users (right now we assume that the number at the end of the username corresponds to the EmployeeID or AdminID)
-CREATE USER 'employee1'@'%' IDENTIFIED BY 'sK244LcP';
-CREATE USER 'employee2'@'%' IDENTIFIED BY 'Rkmeoz4h';
-CREATE USER 'employee3'@'%' IDENTIFIED BY 'cw3txpLt';
-CREATE USER 'admin1'@'%' IDENTIFIED BY 'MbwgpN6z';
-CREATE USER 'admin2'@'%' IDENTIFIED BY 'guxL9FTM';
-CREATE USER 'admin3'@'%' IDENTIFIED BY 'h9RDymfa';
-
--- Create a view that contains all tuples from the Requests table that were created by the user
-CREATE VIEW `MyRequests` AS
-SELECT * 
-FROM `Requests`
-WHERE `CreatedBY` = CAST(REGEXP_REPLACE(SUBSTRING_INDEX(USER(), '@', 1), '[^0-9]', '') AS UNSIGNED);
-
--- Create a view that contains all tuples from the Routes table that are owned by the user
-CREATE VIEW `MyRoutes` AS
-SELECT *
-FROM `Routes`
-WHERE `Owner` = CAST(REGEXP_REPLACE(SUBSTRING_INDEX(USER(), '@', 1), '[^0-9]', '') AS UNSIGNED);
-
--- Grant privileges to employees so that they can view their requests and routes
-GRANT SELECT ON `VPNManagement`.`MyRequests`, `VPNManagement`.`MyRoutes`
-TO 'employee1'@'%', 'employee2'@'%', 'employee3'@'%';
-
--- Grant privileges to employees so that they can create requests
-GRANT INSERT ON `VPNManagement`.`Requests`
-TO 'employee1'@'%', 'employee2'@'%', 'employee3'@'%';
--- Note that the MyRequests view is non-updatable due to the use of the functions in the WHERE clause, so we must grant this privilege on the original table
-
--- Grant all privileges on all tables to the admins (limited to what the executing user can grant)
-GRANT ALL PRIVILEGES ON `VPNManagement`.*
-TO 'admin1'@'%', 'admin2'@'%', 'admin3'@'%';
+SELECT * FROM Routes;
+SELECT * FROM Requests;
+SELECT * FROM Admin;
+SELECT * FROM Employee
