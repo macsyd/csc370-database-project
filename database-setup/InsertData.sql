4f0075402f0692725c73a854020e4521b9b9ba79
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-07-22 13:03:13 -0700

    Add statement to insert user data in InsertData.sql

--- database-setup/InsertData.sql
+++ database-setup/InsertData.sql
@@ -1,9 +1,15 @@
+-- Insert user data
+LOAD DATA LOCAL INFILE '/path/to/User.csv'
+INTO TABLE `User`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
 -- Insert employee data
 LOAD DATA INFILE 'file path'
 INTO TABLE Employee
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
-
 
 -- Insert admin data
 LOAD DATA INFILE 'file path'
@@ -11,13 +17,11 @@ INTO TABLE Admin
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
-
 -- Insert request data
 LOAD DATA INFILE 'file path'
 INTO TABLE Requests
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
-
 
 -- Insert route data
 LOAD DATA INFILE 'file path'
