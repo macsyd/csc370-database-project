51ce3b1b46a432bc7cabab288dd592e0a96652f7
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-07-23 09:23:37 -0700

    Add triggers to VPNManagement.sql

--- database-setup/VPNManagement.sql
+++ database-setup/VPNManagement.sql
@@ -52,3 +52,49 @@ CREATE TABLE Routes (
     FOREIGN KEY (Owner) REFERENCES Employees(UserID),
     CHECK (CreationDate < ExpiryDate)
 );
+
+-- Create triggers to prevent duplicate UserIDs
+
+DELIMITER $$
+
+CREATE TRIGGER employee_userid_check_insert
+BEFORE INSERT ON Employees
+FOR EACH ROW
+BEGIN
+	IF EXISTS (SELECT * FROM Admins WHERE UserID = NEW.UserID) THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'UserID already exists in Admins table';
+	END IF;
+END $$
+
+CREATE TRIGGER employee_userid_check_update
+BEFORE UPDATE ON Employees
+FOR EACH ROW
+BEGIN
+	IF EXISTS (SELECT * FROM Admins WHERE UserID = NEW.UserID) THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'UserID already exists in Admins table';
+	END IF;
+END $$
+
+CREATE TRIGGER admin_userid_check_insert
+BEFORE INSERT ON Admins
+FOR EACH ROW
+BEGIN
+	IF EXISTS (SELECT * FROM Employees WHERE UserID = NEW.UserID) THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'UserID already exists in Employees table';
+	END IF;
+END $$
+
+CREATE TRIGGER admin_userid_check_update
+BEFORE UPDATE ON Admins
+FOR EACH ROW
+BEGIN
+	IF EXISTS (SELECT * FROM Employees WHERE UserID = NEW.UserID) THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'UserID already exists in Employees table';
+	END IF;
+END $$
+
+DELIMITER ;
