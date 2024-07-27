61b6e6b904291d44bf323fc5eabc83c9b81b6769
Jacob Arychuk <156041293+jzarychuk@users.noreply.github.com>
2024-07-26 21:15:34 -0700

    Update SimplifiedSubqueries.sql

--- project-deliverables/sprint-5/SimplifiedSubqueries.sql
+++ project-deliverables/sprint-5/SimplifiedSubqueries.sql
@@ -15,7 +15,7 @@ LEFT OUTER JOIN `Requests`
   ON (`Employees`.`UserID` = `Requests`.`CreatedBy`)
 WHERE `Requests`.`CreatedBy` IS NULL
 ORDER BY UserID;
--- Note: We have a NOT NULL constraint on `Requests`.`CreatedBy` in our schema, so we are safe in that this will not cause spurious results.
+-- Note: We have a NOT NULL constraint on `Requests`.`CreatedBy` in our schema, so it cannot already be NULL and we are safe in that this change will not cause spurious results.
 
 
 /* OLD
@@ -55,4 +55,4 @@ LEFT OUTER JOIN `Admins`
   ON (`Requests`.`ReviewedBy` = `Admins`.`UserID`)
 WHERE `Admins`.`UserID` IS NULL
 AND `ReviewedBy` IS NOT NULL;
--- Note: We have `Admins`.`UserID` set as a primary key in our schema, so it cannot be NULL and we are safe in that this will not cause spurious results.
+-- Note: We have `Admins`.`UserID` set as a primary key in our schema, so it cannot already be NULL and we are safe in that this change will not cause spurious results.
